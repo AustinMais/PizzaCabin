515273c2659ffa72c81b599bb57c128d30b1f767
Austin Mais <austin.mais@gmail.com>
2019-01-19 14:41:05 -0700

    Added JSon custom classes and loops for viewing the data in console.

--- PizzaCabin1/PizzaCabin1/Program.cs
+++ PizzaCabin1/PizzaCabin1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PizzaCabin1
 {
@@ -10,6 +12,21 @@ namespace PizzaCabin1
     {
         static void Main(string[] args)
         {
+            string json;
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString("http://pizzacabininc.azurewebsites.net/PizzaCabinInc.svc/schedule/2015-12-14");
+            }
+            Rootobject example = JsonConvert.DeserializeObject<Rootobject>(json);
+            for(int i = 0; i < example.ScheduleResult.Schedules.Length; i++)
+            {
+                Console.WriteLine(example.ScheduleResult.Schedules[i].Name);
+                for (int j = 0; j < example.ScheduleResult.Schedules[i].Projection.Length; j++)
+                {
+                    Console.WriteLine("  " + example.ScheduleResult.Schedules[i].Projection[j].Description + ": " + example.ScheduleResult.Schedules[i].Projection[j].minutes);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ PizzaCabin1/PizzaCabinTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PizzaCabinTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PizzaCabinTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63cd42d9-bbb5-415a-bee2-40ace14050e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
