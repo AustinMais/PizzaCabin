b2f84ca5a9667b932eccf4eedbff528f1049023c
Austin Mais <austin.mais@gmail.com>
2019-01-22 09:21:51 -0700

    Cleaned up code Added DB Creation Script

---
+++ PizzaCabin1/Database/PizzaDBCreation.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE Schedules
+(
+	[ScheduleId] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[Schedule] INT NOT NULL,
+	[ContractTimeMinutes] INT NOT NULL,
+	[ScheduleDate] DATE NOT NULL,
+	[IsFullDayAbscense] BIT NOT NULL,
+	[EmployeeName] VARCHAR(50) NOT NULL,
+	[PersonID] VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE Projections
+(
+	[ProjectionId] INT NOT NULL IDENTITY(0,1) PRIMARY KEY,
+	[PersonID] VARCHAR(50) NOT NULL,
+	[ScheduleID] INT NOT NULL FOREIGN KEY REFERENCES Schedules(ScheduleID),
+);
+
+CREATE TABLE Activities
+(
+	[ActivityId] INT NOT NULL IDENTITY(0,1) PRIMARY KEY,
+	[ActivityNumber] INT NOT NULL,
+	[Color] VARCHAR(10) NOT NULL,
+	[Description] VARCHAR(200) NOT NULL,
+	[StartDateTime] DATE NOT NULL,
+	[Minutes] INT NOT NULL,
+	[ProjectionID] INT NOT NULL FOREIGN KEY REFERENCES Projections(ProjectionId)
+);
--- PizzaCabin1/PizzaCabin1/Program.cs
+++ PizzaCabin1/PizzaCabin1/Program.cs
@@ -1,46 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Configuration;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-
 namespace PizzaCabin1
 {
     class Program
     {
+        //Main Method
         static void Main(string[] args)
         {
+            Console.WriteLine("Entering Main Method");
+            //Variables
             string json;
+
+            //Call to web service
             //WebServiceCall WebServiceTest = new WebServiceCall();
+            //json = WebServiceTest.ServiceCall();
+
+            //Pulling JSon Data
             using (WebClient wc = new WebClient())
             {
                 json = wc.DownloadString("http://pizzacabininc.azurewebsites.net/PizzaCabinInc.svc/schedule/2015-12-14");
             }
-            //json = WebServiceTest.ServiceCall();
+
+            //Deserializing JSon string and putting it into RootObject
             Rootobject rootobject = JsonConvert.DeserializeObject<Rootobject>(json);
+            Console.WriteLine("Deserialized JSon");
+            //Calling UpdateDB Method
             UpdateDB(rootobject);
+
             Console.ReadLine();
         }
+        //Update DB Method
+        //Takes JSon RootObject and puts data into SQL Database
         public static void UpdateDB(Rootobject rootobject)
         {
+            Console.WriteLine("Entering UpdateDB Method");
+            //Database Creation script located: ..\PizzaCabin\PizzaCabin1\Database\PizzaDBCreation.sql
+            //Variables
             string InsertShedules = "";
             string InsertProjections = "";
             string InsertActvities = "";
+
+            //Setting up SQL Connection
             System.Data.SqlClient.SqlConnection sqlConnection1 =
                 new System.Data.SqlClient.SqlConnection(ConfigurationManager.AppSettings["DBConnectionString"]);
 
             System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
             cmd.CommandType = System.Data.CommandType.Text;
             cmd.Connection = sqlConnection1;
+            Console.WriteLine("SQL Connections Created");
 
+            //Looping through Schedules
             for (int i = 0; i < rootobject.ScheduleResult.Schedules.Length; i++)
             {
                 int IsFullDayAbsence;
+                //Changing IsFullAbscense from Bool to BIT for SQL DB
                 if (rootobject.ScheduleResult.Schedules[i].IsFullDayAbsence == false)
                 {
                     IsFullDayAbsence = 0;
@@ -49,13 +64,14 @@ namespace PizzaCabin1
                 {
                     IsFullDayAbsence = 1;
                 }
+                //Inserting Schedules into Tables
                 InsertShedules = "INSERT INTO Schedules (Schedule, ContractTimeMinutes, ScheduleDate, IsFullDayAbscense, EmployeeName, PersonID) " +
                     " VALUES ("+ i + "," + rootobject.ScheduleResult.Schedules[i].ContractTimeMinutes + ",'" + rootobject.ScheduleResult.Schedules[i].Date + "'," + IsFullDayAbsence + ",'" + rootobject.ScheduleResult.Schedules[i].Name + "','" + rootobject.ScheduleResult.Schedules[i].PersonId + "')";
                 cmd.CommandText = InsertShedules;
                 sqlConnection1.Open();
                 cmd.ExecuteNonQuery();
                 sqlConnection1.Close();
-
+                //Inserting Projections into Tables
                 InsertProjections = "INSERT INTO Projections (PersonID, ScheduleID) " +
                     " VALUES ('" + rootobject.ScheduleResult.Schedules[i].PersonId + "'," + "(SELECT ScheduleID FROM Schedules WHERE Schedule = '" + i + "' AND PersonID = '" + rootobject.ScheduleResult.Schedules[i].PersonId + "'))";
                 cmd.CommandText = InsertProjections;
@@ -63,8 +79,10 @@ namespace PizzaCabin1
                 cmd.ExecuteNonQuery();
                 sqlConnection1.Close();
                 int k = 0;
+                //Looping through Activities
                 foreach (PizzaCabin1.Activity a in rootobject.ScheduleResult.Schedules[i].Projection)
                 {
+                    //Inserting Activities into Tables
                     InsertActvities = "INSERT INTO Activities(ActivityNumber, Color, Description, StartDateTime, Minutes, ProjectionID) " +
                         " VALUES (" + k + ",'" + a.Color + "','" + a.Description + "','" + a.Start + "'," + a.minutes + ",(SELECT ProjectionID FROM Projections WHERE ScheduleId = (SELECT ScheduleID FROM Schedules WHERE Schedule = " + i + " AND PersonID = '" + rootobject.ScheduleResult.Schedules[i].PersonId + "') AND PersonID = '" + rootobject.ScheduleResult.Schedules[i].PersonId + "'))";
 
@@ -75,6 +93,7 @@ namespace PizzaCabin1
                     k++;
                 }
             }
+            Console.WriteLine("SQL Update Completed");
         }
     }
 }
